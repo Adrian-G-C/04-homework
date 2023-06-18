b263f20f4be6bee640134c10321aaff864c4325b
Adrian <121769198+Adi-de-la-Adrian@users.noreply.github.com>
2023-06-17 19:27:37 -0500

    finalized the .js and index.html

--- assets/app.js
+++ assets/app.js
@@ -1,109 +1,118 @@
-// Array to hold the quiz questions
 var questions = [
     {
-      question: "What is the capital of France?",
-      choices: ["Paris", "London", "Berlin", "Madrid"],
-      answer: "Paris"
+      question: "Question 1: Who is Arsenal's all-time leading goal scorer?",
+      options: ["Dennis Bergkamp", "Ian Wright", "Thierry Henry", "Robin van Persie"],
+      answerIndex: 2
     },
     {
-      question: "Who painted the Mona Lisa?",
-      choices: ["Leonardo da Vinci", "Pablo Picasso", "Vincent van Gogh", "Michelangelo"],
-      answer: "Leonardo da Vinci"
+      question: "Question 2: Which manager led Arsenal to their famous 'Invincibles' season?",
+      options: ["Arsène Wenger", "George Graham", "Herbert Chapman", "Bertie Mee"],
+      answerIndex: 0
     },
     {
-      question: "What is the largest planet in our solar system?",
-      choices: ["Jupiter", "Saturn", "Neptune", "Earth"],
-      answer: "Jupiter"
-    }
+      question: "Question 3: Who is the current captain of Arsenal FC?",
+      options: ["Pierre-Emerick Aubameyang", "Bukayo Saka", "Héctor Bellerín", "Granit Xhaka"],
+      answerIndex: 3
+    },
+    {
+      question: "Question 4: Which stadium is the home ground of Arsenal FC?",
+      options: ["Old Trafford", "Anfield", "Emirates Stadium", "Stamford Bridge"],
+      answerIndex: 2
+    },
+    {
+      question: "Question 5: Who is Arsenal's most expensive signing in terms of transfer fee?",
+      options: ["Mesut Özil", "Nicolas Pépé", "Alexandre Lacazette", "Pierre-Emerick Aubameyang"],
+      answerIndex: 1
+    },
   ];
   
-  var currentQuestionIndex = 0;
-  var timeLeft = 60;
-  var timerInterval;
-  var score = 0;
   
-  var startButton = document.getElementById("start-btn");
-  var questionContainer = document.getElementById("question-container");
-  var questionElement = document.getElementById("question");
-  var choicesElement = document.getElementById("choices");
-  var resultContainer = document.getElementById("result-container");
-  var resultElement = document.getElementById("result");
-  var timerElement = document.getElementById("timer");
-  var scoreElement = document.getElementById("score");
-  var initialsElement = document.getElementById("initials");
-  var saveButton = document.getElementById("save-btn");
+  var currentQuestionIndex = 0;
+  var score = 0;
+  var timeLeft = 60;
+  var timerId;
+  
+  var startBtn = document.getElementById("start-btn");
+  var questionNumberSpan = document.getElementById("question-number");
+  var questionText = document.getElementById("question-text");
+  var optionsContainer = document.getElementById("options-container");
+  var feedbackText = document.getElementById("feedback");
+  var endContainer = document.getElementById("end-container");
+  var scoreSpan = document.getElementById("score");
+  var initialsInput = document.getElementById("initials");
+  var saveBtn = document.getElementById("save-btn");
   
   function startQuiz() {
-    startButton.style.display = "none";
-    questionContainer.style.display = "block";
-    timerInterval = setInterval(updateTimer, 1000);
-    showQuestion();
+    var startContainer = document.getElementById("start-container");
+    startContainer.classList.add("hide");
+    var quizContainer = document.getElementById("quiz-container");
+    quizContainer.classList.remove("hide");
+    startTimer();
+    displayQuestion();
   }
   
-  function showQuestion() {
-    var question = questions[currentQuestionIndex];
-    questionElement.textContent = question.question;
-    choicesElement.innerHTML = "";
+  function displayQuestion() {
+    var currentQuestion = questions[currentQuestionIndex];
+    questionNumberSpan.textContent = currentQuestionIndex + 1;
+    questionText.textContent = currentQuestion.question;
   
-    for (var i = 0; i < question.choices.length; i++) {
-      var choice = document.createElement("li");
-      choice.textContent = question.choices[i];
-      choice.setAttribute("data-index", i);
-      choice.addEventListener("click", checkAnswer);
-      choicesElement.appendChild(choice);
-    }
+    optionsContainer.innerHTML = "";
+  
+    currentQuestion.options.forEach(function(option, index) {
+      var optionBtn = document.createElement("button");
+      optionBtn.classList.add("option-btn");
+      optionBtn.textContent = option;
+      optionBtn.addEventListener("click", function() {
+        checkAnswer(index);
+      });
+      optionsContainer.appendChild(optionBtn);
+    });
   }
   
-  function checkAnswer(event) {
-    var selectedChoice = event.target;
-    var selectedAnswer = selectedChoice.textContent;
-    var question = questions[currentQuestionIndex];
-  
-    if (selectedAnswer === question.answer) {
-      resultElement.textContent = "Correct!";
-      score++;
+  function checkAnswer(selectedIndex) {
+    var currentQuestion = questions[currentQuestionIndex];
+    if (selectedIndex === currentQuestion.answerIndex) {
+      score += 10;
+      feedbackText.textContent = "Correct!";
     } else {
-      resultElement.textContent = "Wrong!";
+      score -= 10;
       timeLeft -= 10;
+      feedbackText.textContent = "Wrong!";
     }
   
-    resultContainer.style.display = "block";
+    feedbackText.classList.remove("hide");
     currentQuestionIndex++;
-  
     if (currentQuestionIndex < questions.length) {
-      setTimeout(showQuestion, 1000);
+      setTimeout(displayQuestion, 1000);
     } else {
-      endQuiz();
-    }
-  }
-  
-  function updateTimer() {
-    timeLeft--;
-    timerElement.textContent = "Time: " + timeLeft;
-  
-    if (timeLeft <= 0) {
       endQuiz();
     }
   }
   
   function endQuiz() {
-    clearInterval(timerInterval);
-    questionContainer.style.display = "none";
-    resultContainer.style.display = "none";
-    scoreElement.textContent = "Final Score: " + score;
-    initialsElement.addEventListener("input", enableSaveButton);
-    saveButton.addEventListener("click", saveScore);
-    document.getElementById("initials-container").style.display = "block";
+    clearInterval(timerId);
+    var quizContainer = document.getElementById("quiz-container");
+    quizContainer.classList.add("hide");
+    endContainer.classList.remove("hide");
+    scoreSpan.textContent = score;
   }
   
-  function enableSaveButton() {
-    saveButton.disabled = initialsElement.value === "";
+  function startTimer() {
+    timerId = setInterval(function() {
+      timeLeft--;
+      if (timeLeft >= 0) {
+        document.getElementById("timer").textContent = timeLeft;
+      } else {
+        endQuiz();
+      }
+    }, 1000);
   }
   
-  function saveScore() {
-    var initials = initialsElement.value;
-    // Save the score and initials to the desired location or perform any desired action
-  }
+  startBtn.addEventListener("click", startQuiz);
   
-  startButton.addEventListener("click", startQuiz);
-  
+  saveBtn.addEventListener("click", function() {
+    var initials = initialsInput.value.trim();
+    if (initials !== "") {
+      alert("Score saved!");
+    }
+  });  
