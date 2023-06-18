bd2befdcd8382e850601c396b8693b6b761d238e
Adrian <121769198+Adi-de-la-Adrian@users.noreply.github.com>
2023-06-17 19:06:42 -0500

    adding start button

--- assets/app.js
+++ assets/app.js
@@ -1,119 +1,109 @@
-var quizQuestions = [
+// Array to hold the quiz questions
+var questions = [
     {
-      question: "Question 1: What is the capital of France?",
-      choices: ["Paris", "Madrid", "Rome", "London"],
-      correctAnswer: "Paris"
+      question: "What is the capital of France?",
+      choices: ["Paris", "London", "Berlin", "Madrid"],
+      answer: "Paris"
     },
     {
-      question: "Question 2: What is the largest planet in our solar system?",
-      choices: ["Jupiter", "Saturn", "Neptune", "Mars"],
-      correctAnswer: "Jupiter"
+      question: "Who painted the Mona Lisa?",
+      choices: ["Leonardo da Vinci", "Pablo Picasso", "Vincent van Gogh", "Michelangelo"],
+      answer: "Leonardo da Vinci"
     },
     {
-      question: "Question 3: What is the symbol for the chemical element oxygen?",
-      choices: ["O", "C", "H", "N"],
-      correctAnswer: "O"
-    },
-    {
-      question: "Question 4: Who is the author of the Harry Potter book series?",
-      choices: ["J.K. Rowling", "Stephen King", "George R.R. Martin", "J.R.R. Tolkien"],
-      correctAnswer: "J.K. Rowling"
-    },
-    {
-      question: "Question 5: Which country won the FIFA World Cup in 2018?",
-      choices: ["France", "Brazil", "Germany", "Argentina"],
-      correctAnswer: "France"
+      question: "What is the largest planet in our solar system?",
+      choices: ["Jupiter", "Saturn", "Neptune", "Earth"],
+      answer: "Jupiter"
     }
   ];
   
-  
-  var startBtn = document.getElementById("start-btn");
-  var quizScreen = document.getElementById("quiz-screen");
-  var questionText = document.getElementById("question-text");
-  var choicesContainer = document.getElementById("choices");
-  var message = document.getElementById("message");
-  var nextBtn = document.getElementById("next-btn");
-  var gameOverScreen = document.getElementById("game-over-screen");
-  var finalScore = document.getElementById("final-score");
-  var initialsForm = document.getElementById("initials-form");
-  var initialsInput = document.getElementById("initials");
-  
   var currentQuestionIndex = 0;
-  var timer;
-  var timeLeft;
+  var timeLeft = 60;
+  var timerInterval;
   var score = 0;
   
-  startBtn.addEventListener("click", startQuiz);
-  nextBtn.addEventListener("click", showNextQuestion);
-  initialsForm.addEventListener("submit", saveScore);
+  var startButton = document.getElementById("start-btn");
+  var questionContainer = document.getElementById("question-container");
+  var questionElement = document.getElementById("question");
+  var choicesElement = document.getElementById("choices");
+  var resultContainer = document.getElementById("result-container");
+  var resultElement = document.getElementById("result");
+  var timerElement = document.getElementById("timer");
+  var scoreElement = document.getElementById("score");
+  var initialsElement = document.getElementById("initials");
+  var saveButton = document.getElementById("save-btn");
   
   function startQuiz() {
-    startBtn.style.display = "none";
-    quizScreen.style.display = "block";
-    timeLeft = 60;
-    score = 0;
-    displayQuestion();
-    startTimer();
+    startButton.style.display = "none";
+    questionContainer.style.display = "block";
+    timerInterval = setInterval(updateTimer, 1000);
+    showQuestion();
   }
   
-  function displayQuestion() {
-    var currentQuestion = quizQuestions[currentQuestionIndex];
-    questionText.textContent = currentQuestion.question;
-    choicesContainer.innerHTML = "";
+  function showQuestion() {
+    var question = questions[currentQuestionIndex];
+    questionElement.textContent = question.question;
+    choicesElement.innerHTML = "";
   
-    for (var i = 0; i < currentQuestion.choices.length; i++) {
-      var choice = currentQuestion.choices[i];
-      var choiceButton = document.createElement("button");
-      choiceButton.textContent = choice;
-      choiceButton.addEventListener("click", function() {
-        checkAnswer(choice);
-      });
-      choicesContainer.appendChild(choiceButton);
+    for (var i = 0; i < question.choices.length; i++) {
+      var choice = document.createElement("li");
+      choice.textContent = question.choices[i];
+      choice.setAttribute("data-index", i);
+      choice.addEventListener("click", checkAnswer);
+      choicesElement.appendChild(choice);
     }
   }
   
-  function checkAnswer(selectedChoice) {
-    var currentQuestion = quizQuestions[currentQuestionIndex];
-    if (selectedChoice === currentQuestion.correctAnswer) {
+  function checkAnswer(event) {
+    var selectedChoice = event.target;
+    var selectedAnswer = selectedChoice.textContent;
+    var question = questions[currentQuestionIndex];
+  
+    if (selectedAnswer === question.answer) {
+      resultElement.textContent = "Correct!";
       score++;
-      message.textContent = "Correct!";
     } else {
-      timeLeft -= 10; 
-      message.textContent = "Incorrect!";
+      resultElement.textContent = "Wrong!";
+      timeLeft -= 10;
     }
   
-    if (currentQuestionIndex === quizQuestions.length - 1) {
+    resultContainer.style.display = "block";
+    currentQuestionIndex++;
+  
+    if (currentQuestionIndex < questions.length) {
+      setTimeout(showQuestion, 1000);
+    } else {
       endQuiz();
-    } else {
-      currentQuestionIndex++;
-      nextBtn.style.display = "block";
     }
   }
   
-  function showNextQuestion() {
-    nextBtn.style.display = "none";
-    message.textContent = "";
-    displayQuestion();
-  }
+  function updateTimer() {
+    timeLeft--;
+    timerElement.textContent = "Time: " + timeLeft;
   
-  function startTimer() {
-    timer = setInterval(function() {
-      timeLeft--;
-      if (timeLeft <= 0) {
-        endQuiz();
-      }
-    }, 1000);
+    if (timeLeft <= 0) {
+      endQuiz();
+    }
   }
   
   function endQuiz() {
-    clearInterval(timer);
-    quizScreen.style.display = "none";
-    gameOverScreen.style.display = "block";
-    finalScore.textContent = score;
+    clearInterval(timerInterval);
+    questionContainer.style.display = "none";
+    resultContainer.style.display = "none";
+    scoreElement.textContent = "Final Score: " + score;
+    initialsElement.addEventListener("input", enableSaveButton);
+    saveButton.addEventListener("click", saveScore);
+    document.getElementById("initials-container").style.display = "block";
   }
   
-  function saveScore(event) {
-    event.preventDefault();
-    var initials = initialsInput.value;
+  function enableSaveButton() {
+    saveButton.disabled = initialsElement.value === "";
   }
+  
+  function saveScore() {
+    var initials = initialsElement.value;
+    // Save the score and initials to the desired location or perform any desired action
+  }
+  
+  startButton.addEventListener("click", startQuiz);
   
